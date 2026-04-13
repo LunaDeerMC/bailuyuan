036e4aadbb1a3362a7136745d650ddbe68f55aa2
zhangyuheng <zhangyuheng@lunadeer.cn>
2026-04-13 17:23:58 +0800

    feat: update .gitignore and configure HTTPS settings in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import fs from 'fs';
 
 export default defineConfig({
   plugins: [vue()],
@@ -11,4 +12,10 @@ export default defineConfig({
       return paths.filter((path) => !path.endsWith('.html'));
     },
   },
+  server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    }
+  }
 });
